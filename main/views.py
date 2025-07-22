21c71609991d7bbcd39a9fe5aad66c5a3f4b18fd
Umair Saeed <umairsaeed320@gmail.com>
2025-07-21 21:54:12 -0700

    Update views.py

--- main/views.py
+++ main/views.py
@@ -32,9 +32,9 @@ u1 = get_user_model()
 u11 = settings.AUTH_USER_MODEL 
 
 
-#model = SentenceTransformer('all-MiniLM-L6-v2')  # or any of the models above
+model = SentenceTransformer('all-MiniLM-L6-v2')  # or any of the models above
 #model = SentenceTransformer('stsb-roberta-large')
-model = SentenceTransformer('all-mpnet-base-v2')  # ~420MB but ~85 STS accuracy
+#model = SentenceTransformer('all-mpnet-base-v2')  # ~420MB but ~85 STS accuracy
 #model = SentenceTransformer('D:\Project\stsb-roberta-large1')  # ~60MB but ~80 STS accuracy
 
 def extract_text(file_path):
